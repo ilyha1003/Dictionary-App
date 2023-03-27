73f15951bd15cb6a30e221f8e0801d9cc6bf5890
Sankovich Ilya <ilyhasankovich@gmail.com>
2023-03-27 23:43:13 +0300

    inintial commit

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,11 +1,38 @@
-const getApiData = async (inputValue) => {
-    try {
-        const data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`);
-        const json = await data.json();
+const list = document.querySelector('.word__list');
+const input = document.querySelector('.input');
+const main = document.querySelector('.main');
+
+const debounce = (func, delay) => {
+    let timeout;
+    return function () {
+        const funcCall = () => {
+            func.apply(this, arguments)
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(funcCall, delay);       
+    };
+}
+
+const getApiData = async () => {
+    list.innerHTML = ''
+    const inputValue = input.value;
+    const data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`);
+    const json = await data.json();
+
+    if(inputValue) {
+        if(json.title) {
+            console.log(json.title); 
+            const errorElement = document.createElement('li');
+            errorElement.innerText = json.title;
+            list.append(errorElement)       
+        }
         console.log(json);
-    } catch(error) {
-        console.log('Error >>>', error);
+        return json;    
+    } else {
+        list.innerHTML = '';
     }
 }
 
-export default getApiData
+const getData = debounce(getApiData, 500);
+
+export default getData;
--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,10 @@
-import getApiData from "./Api";
+import getData from "./api";
 import { loaderHTML } from "./constants";
 
 document.addEventListener('DOMContentLoaded', () => {
     const input = document.querySelector('.input');
     const wordList = document.querySelector('.word__list');
-
-    const debounce = (func, delay) => {
-        let timeout;
-        return function () {
-            const funcCall = () => {
-                func.apply(this, arguments)
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(funcCall, delay);
-        };
-    }
-
-    const getApiData = async () => {
-        const inputValue = input.value;
-        if(inputValue) {
-            const data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`);
-            const json = await data.json();
-            console.log(json);
-        }
-        wordList.innerHTML = '';
-    }
-
-    getData = debounce(getApiData, 500);
+    input.focus();
 
     input.oninput = () => {
         wordList.innerHTML = loaderHTML;
